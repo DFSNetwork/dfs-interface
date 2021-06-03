fbc0d9d0d689a24b960016204377318b2104b175
webWorker-eos <1574533602@qq.com>
2021-06-03 09:01:13 +0800

    bug

--- src/views/swap/comp/SwapInfo.vue
+++ src/views/swap/comp/SwapInfo.vue
@@ -293,6 +293,7 @@ export default {
         rSwitch: this.rSwitch,
       }
       const out = getAmtOut(SwapRouter, inData)
+      console.log(out)
       if (!out || !out.quantity_out) {
         this.$emit('listenTradeInfo', {
           show: '',
@@ -303,11 +304,11 @@ export default {
       this.outPrice = toFixed(out.swapOutPrice, Number(this.tokenA.decimal) + 2)
 
       // 有该交易对
-      this.hasMids = type === 'buy' ? out.hasMids : this.handleReverse(out.hasMids);
+      this.hasMids = type === 'pay' ? out.hasMids : this.handleReverse(out.hasMids);
       // 最优兑换路径
-      this.bestPath = type === 'buy' ? out.bestPath : this.handleReverse(out.bestPath);
+      this.bestPath = type === 'pay' ? out.bestPath : this.handleReverse(out.bestPath);
       // 兑换路径 - mids
-      this.mids = type === 'buy' ? out.mid : this.handleReverse(out.mid);
+      this.mids = type === 'pay' ? out.mid : this.handleReverse(out.mid);
 
       if (!parseFloat(this.pay || 0) && !parseFloat(this.get || 0)) {
         this.$emit('listenTradeInfo', {
