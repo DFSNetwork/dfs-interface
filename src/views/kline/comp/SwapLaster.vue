ce36a0c13b742ad2b26ae1cb8b6d8d820f3db910
webWorker-eos <1574533602@qq.com>
2021-06-21 23:05:35 +0800

    update

--- src/views/kline/comp/SwapLaster.vue
+++ src/views/kline/comp/SwapLaster.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-    <div class="list" v-for="(v, i) in lasters" :key="i">
-      <div class="flexb info">
-        <span class="name">{{ v.user }}</span>
-        <span class="dinReg">{{ v.time }}</span>
-      </div>
-      <div class="flexb">
-        <span>{{ $t('kline.swap') }}</span>
-        <div class="flexa dinReg">
-          <span>{{ v.qin }}</span>
-          <img class="swapLasterImg" src="https://cdn.jsdelivr.net/gh/defis-net/material2/dfs/swapLaster.png" alt="">
-          <span>{{ v.qout }}</span>
+    <van-list
+        v-model="loadingMore"
+        :finished="finished"
+        :loading-text="$t('public.loading')"
+        :finished-text="$t('public.noMore')"
+        @load="handleCurrentChange"
+      >
+      <div class="list" v-for="(v, i) in lasters" :key="i">
+        <div class="flexb info">
+          <span class="name">{{ v.user }}</span>
+          <span class="dinReg">{{ v.time }}</span>
+        </div>
+        <div class="flexb">
+          <span>{{ $t('kline.swap') }}</span>
+          <div class="flexa dinReg">
+            <span>{{ v.qin }}</span>
+            <img class="swapLasterImg" src="https://cdn.jsdelivr.net/gh/defis-net/material2/dfs/swapLaster.png" alt="">
+            <span>{{ v.qout }}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </van-list>
+    
   </div>
 </template>
 
@@ -34,28 +43,34 @@ export default {
     return {
       lasters: [],
       timer: null,
+
+      loadingMore: false,
+      finished: false,
+      page: 1,
     }
   },
   beforeDestroy() {
     clearTimeout(this.timer)
   },
   watch: {
-    checkedMarket: {
-      handler: function cm(newVal) {
-        if (!newVal.mid) {
-          return
-        }
-        this.handleGetLaster()
-      },
-      deep: true,
-      immediate: true,
-    }
+    // checkedMarket: {
+    //   handler: function cm(newVal) {
+    //     if (!newVal.mid) {
+    //       return
+    //     }
+    //     this.handleGetLaster()
+    //   },
+    //   deep: true,
+    //   immediate: true,
+    // }
   },
   methods: {
     async handleGetLaster() {
       clearTimeout(this.timer)
       const params = {
-        mid: this.checkedMarket.mid
+        mid: this.checkedMarket.mid,
+        page: this.page,
+        limit: 30
       }
       const {status, result} = await this.$api.get_swap_lasters(params);
       this.timer = setTimeout(() => {
@@ -70,8 +85,26 @@ export default {
         const time = toLocalTime(v.create_time)
         v.time = time;
       });
-      this.lasters = rows;
-    }
+      if (this.page === 1) {
+        this.lasters = rows;
+      } else {
+        this.lasters.push(...rows);
+      }
+      this.loadingMore = false;
+      this.page += 1;
+      if (rows.length < 30) {
+        this.finished = true;
+      }
+    },
+    handleCurrentChange() {
+      if (!this.checkedMarket.mid) {
+        setTimeout(() => {
+          this.handleCurrentChange()
+        }, 500);
+        return
+      }
+      this.handleGetLaster()
+    },
   }
 }
 </script>
