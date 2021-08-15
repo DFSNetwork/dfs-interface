2a8bba6a142f7dbecd87d627b5423409e89b549c
webWorker-eos <1574533602@qq.com>
2021-08-15 11:36:06 +0800

    bug

--- src/views/invite/childViews/Detail.vue
+++ src/views/invite/childViews/Detail.vue
@@ -139,8 +139,7 @@
             <span class="tip ml">(${{ abledRewardU }})</span>
           </div>
         </div>
-        <div class="claimBtn flexc" v-if="!accSnapshoots.owner || !accSnapshoots.is_claim" @click="handleClaim">{{ $t('invite.claim') }}</div>
-        <div></div>
+        <div class="claimBtn flexc" v-if="accSnapshoots.owner === account.name && !accSnapshoots.is_claim" @click="handleClaim">{{ $t('invite.claim') }}</div>
       </div>
     </div>
 
